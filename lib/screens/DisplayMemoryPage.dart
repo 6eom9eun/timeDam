c77a164ec9d64440686b3eda2033b3ffbd16eeef
beomgeun <ggoon98@gmail.com>
2023-11-07 22:59:19 +0900

    생성된 이미지 storage 업로드

--- lib/screens/DisplayMemoryPage.dart
+++ lib/screens/DisplayMemoryPage.dart
@@ -1,38 +1,90 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:memo_re/utils/vars.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+import 'package:http/http.dart' as http;
 
 class DisplayMemoryPage extends StatelessWidget {
   final String memory;
   final String imageUrl;
 
+  // 텍스트와 이미지 URL을 받아 객체를 초기화
   DisplayMemoryPage({required this.memory, required this.imageUrl});
 
+  // 이미지 파일을 인터넷에서 다운로드, 로컬 디바이스에 저장
+  Future<File> _downloadImage(String url) async {
+    final response = await http.get(Uri.parse(url));
+    final documentDirectory = await getApplicationDocumentsDirectory();
+    final file = File(path.join(documentDirectory.path, 'imagename.jpg')); // 이미지 파일의 경로를 지정합니다.
 
+    file.writeAsBytesSync(response.bodyBytes);
+    return file;
+  }
+
+  // 사용자의 추억과 이미지를 Firebase에 업로드
   Future uploadMemoryToFirebase(String memory, String imageUrl) async {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+
+    // 로그인 확인
+    if (user == null) {
+      print('Error: User is not logged in');
+      return;
+    }
+
+    final imageFile = await _downloadImage(imageUrl); // 이미지 다운
+
+    final uid = user.uid; // 사용자 uid
+
+    // 파일 X 에러
+    if (!await imageFile.exists()) {
+      print('Error: Image file does not exist.');
+      return;
+    }
+
+    // Firebase Storage에 저장될 파일의 이름
+    final fileName = 'images/${uid}_${DateTime.now().millisecondsSinceEpoch}.jpg';
+    final storageRef = FirebaseStorage.instance.ref().child(fileName);
+
     try {
+      // 파일을 Firebase Storage에 업로드, 모니터링
+      final uploadTask = storageRef.putFile(imageFile);
 
-      final uid = FirebaseAuth.instance.currentUser?.uid;
-      if (uid == null) { // 회원 아니면 X
-        print('Error: User is not logged in');
-        return;
-      }
+      uploadTask.snapshotEvents.listen(
+            (TaskSnapshot snapshot) {
+          print('Task state: ${snapshot.state}');
+          print('Progress: ${(snapshot.bytesTransferred / snapshot.totalBytes) * 100} %');
+        },
+        onError: (e) {
+          print(uploadTask.snapshot);
 
-      // Firestore에 게시물 정보들을 저장
-      DocumentReference postRef = await FirebaseFirestore.instance.collection('posts').add({
-        'imageUrl': imageUrl,
+          // 권한 오류가 발생한 경우 메시지를 출력
+          if (e.code == 'permission-denied') {
+            print('User does not have permission to upload to this reference.');
+          }
+        },
+      );
+
+      // 업로드가 완료될 때까지 대기
+      final snapshot = await uploadTask;
+      final downloadUrl = await snapshot.ref.getDownloadURL();
+
+      // 업로드된 이미지의 URL과 함께 Firestore에 업로드
+      final postRef = FirebaseFirestore.instance.collection('posts').doc();
+      await postRef.set({
+        'imageUrl': downloadUrl,
         'uid': uid,
         'text': memory,
         'createdAt': FieldValue.serverTimestamp(),
+        'postId': postRef.id,
       });
 
-      // 생성된 문서 ID(postId)를 가져와서 방금 생성한 문서에 업데이트
-      String postId = postRef.id;
-      await postRef.update({'postId': postId});
-
-      print('Memory uploaded to Firebase Firestore! Memory: $memory, ImageURL: $imageUrl, PostID: $postId');
+      print('Memory uploaded to Firebase Firestore and Storage! Memory: $memory, ImageURL: $downloadUrl');
     } catch (e) {
       print('uploadMemoryToFirebase error: $e');
     }
