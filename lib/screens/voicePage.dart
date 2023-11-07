58a47af103ccb9a677839fe91b165acad7904881
beomgeun <ggoon98@gmail.com>
2023-11-07 21:04:48 +0900

    stt

--- lib/screens/voicePage.dart
+++ lib/screens/voicePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:memo_re/utils/vars.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class VoiceInputPage extends StatefulWidget {
   @override
@@ -9,7 +9,7 @@ class VoiceInputPage extends StatefulWidget {
 }
 
 class _VoiceInputPageState extends State<VoiceInputPage> {
-  final SpeechToText speech = SpeechToText();
+  final stt.SpeechToText speech = stt.SpeechToText();
   bool _speechInitialized = false;
   String inputText = "";
 
@@ -21,13 +21,22 @@ class _VoiceInputPageState extends State<VoiceInputPage> {
 
   Future<void> initializeSpeechRecognition() async {
     if (!_speechInitialized) {
-      _speechInitialized = await speech.initialize(onError: (error) {
-        // 에러 처리 로직
-        _speechInitialized = false;
-      });
-      setState(() {});
-      if (_speechInitialized) {
+      bool available = await speech.initialize(
+        onError: (val) {
+          print('Error in speech initialize: $val');
+          setState(() {
+            _speechInitialized = false;
+          });
+        },
+        onStatus: (val) {
+          print('Speech recognition status: $val');
+        },
+      );
+      if (available) {
+        setState(() => _speechInitialized = true);
         startListening();
+      } else {
+        setState(() => _speechInitialized = false);
       }
     }
   }
@@ -35,16 +44,17 @@ class _VoiceInputPageState extends State<VoiceInputPage> {
   Future<void> startListening() async {
     if (speech.isListening) {
       await speech.stop();
+      print('Stopped listening.');
+    } else {
+      await speech.listen(
+        onResult: (result) {
+          setState(() {
+            inputText = result.recognizedWords;
+          });
+          print('Input text: $inputText'); // stt 확인
+        },
+      );
     }
-
-    await speech.listen(
-      onResult: (result) {
-        setState(() {
-          inputText = result.recognizedWords;
-          print(inputText); // stt 확인
-        });
-      },
-    );
   }
 
   @override
@@ -54,7 +64,7 @@ class _VoiceInputPageState extends State<VoiceInputPage> {
   }
 
   @override
-    Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -67,54 +77,55 @@ class _VoiceInputPageState extends State<VoiceInputPage> {
       ),
       backgroundColor: AppColors.backColor(),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 70),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Image.asset('assets/logo.png', width: 80, height: 80),
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Text(
-                '추억을 말해주세요!',
+        child: SingleChildScrollView( // Ensuring the content is scrollable
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 70),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Image.asset('assets/logo.png', width: 80, height: 80),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  '추억을 말해주세요!',
+                  style: TextStyle(
+                    fontFamily: 'Cafe',
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              SizedBox(height: 120),
+              SpinKitWave(
+                color: Color(0xFFFFCF52),
+                size: 200.0,
+              ),
+              SizedBox(height: 50),
+              Text(
+                '음성인식 중입니다..',
                 style: TextStyle(
-                  fontFamily: 'Cafe',
-                  fontSize: 30,
+                  fontFamily: 'CafeAir',
+                  fontSize: 18,
+                  color: Color(0xff777777),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  inputText,
+                  style: TextStyle(
+                    fontFamily: 'CafeAir',
+                    fontSize: 18,
+                    color: Color(0xff777777),
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
-            SizedBox(height: 120),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                SpinKitWave(
-                  color: Color(0xFFFFCF52),
-                  size: 200.0,
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Text(
-              '음성인식 중입니다..',
-              style: TextStyle(
-                fontFamily: 'CafeAir',
-                fontSize: 18,
-                color: Color(0xff777777),
-              ),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              inputText,
-              style: TextStyle(
-                fontFamily: 'CafeAir',
-                fontSize: 18,
-                color: Color(0xff777777),
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
