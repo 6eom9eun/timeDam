2df90881cdeed34a5d344f29d3cc53bc437665ee
beomgeun <ggoon98@gmail.com>
2023-11-10 09:57:00 +0900

    달력 해당 날짜 게시물 표시

--- lib/screens/memoryPage.dart
+++ lib/screens/memoryPage.dart
@@ -3,6 +3,8 @@ import 'package:memo_re/widgets/calendar2_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:memo_re/providers/postProvider.dart';
 import 'package:memo_re/utils/vars.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MemoryPage extends StatefulWidget {
   const MemoryPage({super.key});
@@ -14,12 +16,51 @@ class MemoryPage extends StatefulWidget {
 class _MemoryPageState extends State<MemoryPage> {
   DateTime _focusedDay = DateTime.now();
   DateTime _selectedDay = DateTime.now();
+  Map<DateTime, List<dynamic>> _events = {};
+  List<dynamic> _selectedEvents = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts(_selectedDay); // 초기화할 때 오늘 날짜의 게시물을 가져옵니다.
+  }
+
+  // 이제 _fetchPosts는 특정 날짜의 게시물만 가져옵니다.
+  void _fetchPosts(DateTime date) async {
+    String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+    DateTime dateKey = DateTime(date.year, date.month, date.day);
+    try {
+      var querySnapshot = await FirebaseFirestore.instance
+          .collection('user')
+          .doc(userId)
+          .collection('posts')
+          .where('createdAt', isGreaterThanOrEqualTo: Timestamp.fromDate(dateKey))
+          .where('createdAt', isLessThan: Timestamp.fromDate(dateKey.add(Duration(days: 1))))
+          .get();
+
+      List<dynamic> newEvents = [];
+      for (var doc in querySnapshot.docs) {
+        newEvents.add(doc.data());
+      }
+      if (mounted) {
+        setState(() {
+          _events[dateKey] = newEvents;
+          _selectedEvents = newEvents;
+        });
+      }
+    } catch (e) {
+      // Handle any errors here
+      print(e);
+    }
+  }
 
   void _onDaySelected(DateTime selectedDay) {
     setState(() {
       _selectedDay = selectedDay;
-      _focusedDay = selectedDay;
+      _focusedDay = selectedDay; // You may still want to update _focusedDay if necessary
+      _selectedEvents = _events[selectedDay] ?? [];
     });
+    _fetchPosts(selectedDay); // Fetch posts for the selected day
   }
 
   @override
@@ -33,7 +74,40 @@ class _MemoryPageState extends State<MemoryPage> {
             Calendar2(
               focusedDay: _focusedDay,
               selectedDay: _selectedDay,
-              onDaySelected: _onDaySelected,
+              onDaySelected: (selectedDay) {
+                _onDaySelected(selectedDay);
+              },
+            ),
+            Expanded(
+              child: _selectedEvents.isNotEmpty
+                  ? ListView.builder(
+                itemCount: _selectedEvents.length,
+                itemBuilder: (context, index) {
+                  final event = _selectedEvents[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(event['text'] ?? 'No Content'),
+                      subtitle: Text(
+                        (event['createdAt'] as Timestamp).toDate().toString(),
+                      ),
+                      trailing: event['imageUrl'] != null
+                          ? Image.network(
+                        event['imageUrl'],
+                        fit: BoxFit.cover,
+                        width: 100,
+                        height: 100,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(Icons.error);
+                        },
+                      )
+                          : null,
+                    ),
+                  );
+                },
+              )
+                  : Center(
+                child: Text('No events found for this day.'),
+              ),
             ),
           ],
         ),
