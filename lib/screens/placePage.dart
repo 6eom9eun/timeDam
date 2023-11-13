6d99066cbc6eba73c161958f703f4f40ad306ba1
beomgeun <ggoon98@gmail.com>
2023-11-14 00:27:42 +0900

    svg -> json, lottie 패키지로 애니메이션 아이콘 추가

--- lib/screens/placePage.dart
+++ lib/screens/placePage.dart
@@ -19,7 +19,7 @@ class _PlacePageState extends State<PlacePage> {
     '01d': '날씨가 좋아요,\n추억 만들어봐요.',
     '01n': '날씨가 좋은 밤이네요,\n추억을 만드셨나요?',
     '02d': '날씨가 좋아요,\n추억 만들어봐요.',
-    '01n': '날씨가 좋은 밤이네요,\n추억을 만드셨나요?',
+    '02n': '날씨가 좋은 밤이네요,\n추억을 만드셨나요?',
     '03d': '하늘이 조금 흐려요,\n추억 만들어봐요.',
     '03n': '하늘이 조금 흐린 밤이네요,\n추억을 만드셨나요?.',
     '04d': '하늘이 흐려요,\n추억 만들어봐요.',
@@ -87,31 +87,31 @@ class _PlacePageState extends State<PlacePage> {
                 if (weatherIcon == '01n') // clear sky
                   Lottie.asset('assets/lottie/clear-night.json', width: 100, height: 100),
                 if (weatherIcon == '02d') // few clouds
-                  Lottie.asset('assets/svg/partly-cloudy-day.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/partly-cloudy-day.json', width: 100, height: 100),
                 if (weatherIcon == '02n') // few clouds
-                  Lottie.asset('assets/svg/partly-cloudy-night.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/partly-cloudy-night.json', width: 100, height: 100),
                 if (weatherIcon == '03d' || weatherIcon == '03n') // 	scattered clouds
-                  Lottie.asset('assets/svg/partly-cloudy-night.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/partly-cloudy-night.json', width: 100, height: 100),
                 if (weatherIcon == '04d') // broken clouds
-                  Lottie.asset('assets/svg/extreme-day.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/extreme-day.json', width: 100, height: 100),
                 if (weatherIcon == '04n') // broken clouds
-                  Lottie.asset('assets/svg/extreme-night.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/extreme-night.json', width: 100, height: 100),
                 if (weatherIcon == '09d') // shower rain
-                  Lottie.asset('assets/svg/extreme-rain.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/extreme-rain.json', width: 100, height: 100),
                 if (weatherIcon == '09n') // shower rain
-                  Lottie.asset('assets/svg/extreme-night-rain.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/extreme-night-rain.json', width: 100, height: 100),
                 if (weatherIcon == '10d') // rain
-                  Lottie.asset('assets/svg/overcast-day-rain.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/overcast-day-rain.json', width: 100, height: 100),
                 if (weatherIcon == '10n') // rain
-                  Lottie.asset('assets/svg/overcast-night-rain.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/overcast-night-rain.json', width: 100, height: 100),
                 if (weatherIcon == '11d') // thunderstorm
-                  Lottie.asset('assets/svg/thunderstorms-extreme.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/thunderstorms-extreme.json', width: 100, height: 100),
                 if (weatherIcon == '11n') // thunderstorm
-                  Lottie.asset('assets/svg/thunderstorms-night-extreme.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/thunderstorms-night-extreme.json', width: 100, height: 100),
                 if (weatherIcon == '13d' || weatherIcon == '13n') // snow
-                  Lottie.asset('assets/svg/snow.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/snow.json', width: 100, height: 100),
                 if (weatherIcon == '50d' || weatherIcon == '50n') // mist
-                  Lottie.asset('assets/svg/mist.json', width: 100, height: 100),
+                  Lottie.asset('assets/lottie/mist.json', width: 100, height: 100),
                 SizedBox(width: 20),
                 Expanded(
                   child: Text(weatherText[weatherIcon] ?? '',
